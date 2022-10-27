0125137c966e55966a2e179e9d63cb62d858993b
Kacper Pasierb <w65562@student.wsiz.edu.pl>
2022-10-27 13:06:32 +0200

    lab3 init

---
+++ Controllers/Lab3/ShapeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Services.Lab3;
+
+namespace WebApplication1.Controllers.Lab3
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class ShapeController : ControllerBase
+    {
+        [HttpGet]
+        public int GetArea()
+        {
+            var rectangle = new Rectangle(23, 23);
+            return rectangle.GetArea();
+        }
+        public int GetSumArea()
+        {
+            var rectangle = new Rectangle(3, 3);
+            var rectangle2 = new Rectangle(4, 4);
+            return rectangle + rectangle2;
+        }
+    }
+}
