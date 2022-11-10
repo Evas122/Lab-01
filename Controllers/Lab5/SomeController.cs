9e7fdb3662ecdb1cbca6c659a4e84b23adca39c3
Kacper Pasierb <w65562@student.wsiz.edu.pl>
2022-11-10 13:05:20 +0100

    Third commit

---
+++ Controllers/Lab5/SomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Services.Lab5;
+
+namespace WebApplication1.Controllers.Lab5
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class SomeController : ControllerBase
+    {
+        [HttpGet]
+        public string CallMethod()
+        {
+            var someInstance = new SomeClass();
+            return someInstance.Print(null);
+        }
+    }
+}
---
+++ Services/Lab5/exceptions.cs
@@ -0,0 +1,21 @@
+﻿namespace WebApplication1.Services.Lab5
+{
+    using System;
+
+    public class EmployeeListNotFoundException : Exception
+    {
+        public EmployeeListNotFoundException()
+        {
+        }
+
+        public EmployeeListNotFoundException(string message)
+            : base(message)
+        {
+        }
+
+        public EmployeeListNotFoundException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
